12fc49ddb9bc5aadf73c3410d7237bad35bbcdcd
reggie peled <revital.peled@gmail.com>
2017-07-11 08:42:17 -0400

    add event listener for form submission

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var submission = document.getElementById('submit');
+
+function writeToScreen(eventInfo) {
+    var first = document.getElementById('firstname').value;
+    var last = document.getElementById('lastname').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    eventInfo.preventDefault();
+
+    console.log('The first Name is: ' + first);
+    console.log('The last Name is: ' + last);
+    console.log('The email is: ' + email);
+    console.log('The message is: ' + message);
+}
+submission.addEventListener('click', writeToScreen);
