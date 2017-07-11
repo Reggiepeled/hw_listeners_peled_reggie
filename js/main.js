5de90b1ef2921dadb68259effdd60419c8ff81d8
reggie peled <revital.peled@gmail.com>
2017-07-11 15:34:03 -0400

    add event listener for hovering over page heading

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Click on each image changes the page background:
+
 //get the image element
 var wonderwoman = document.getElementById('wonderwoman');
 var exhaustedmom = document.getElementById('exhaustedmom');
@@ -14,3 +16,18 @@ function changeBackground(eventInfo) {
 //listen to a click on one of the images
 wonderwoman.addEventListener('click', changeBackground);
 exhaustedmom.addEventListener('click', changeBackground);
+
+//-------------------------------------------------------------
+
+//hover over my name creates a new paragraph
+
+//get the h1 element
+var heading = document.getElementById('myName');
+
+function addPharagraph() {
+    var newP = document.createElement('p');
+    newP.innerHTML = '<p>Yep, this is my name.</p></br>';
+    heading.after(newP);
+}
+
+heading.addEventListener('mouseover', addPharagraph);
